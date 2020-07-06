26af51cf61ed3bbb5c1983c840b887b96def5bd3
Geniuska Geniusovna <eminizinar@gmail.com>
2020-07-06 11:16:57 +0300

    Transactions modified

--- cmd/card/main.go
+++ cmd/card/main.go
@@ -2,31 +2,36 @@ package main
 
 import (
 	"fmt"
-	"sort"
 	"github.com/Geniuskaa/task5.1/pkg/transactions"
+	"time"
 )
 
 func main() {
-	const users = 40
-	const transactionsPerUser = 10
-	const transactionsAmount = 1_00
 
 
-	transactionsOfService := make([]transactions.TransactionInfo, users * transactionsPerUser) //всего 400
 
-	transactions.DateAdding(0,80,transactionsOfService,1) //80
-	transactions.DateAdding(80,140,transactionsOfService,8) //60
-	transactions.DateAdding(140,240,transactionsOfService,2) // 100
-	transactions.DateAdding(240,300,transactionsOfService,11) // 60
-	transactions.DateAdding(300,400,transactionsOfService,6)  // 100
+	tranz := []transactions.Transaction{
+		{time.Date(2020, 3, 1, 0, 0, 0, 0, time.Local), 20_396_00},
+		{time.Date(2020, 4, 1, 0, 0, 0, 0, time.Local), 245_00},
+		{time.Date(2020, 5, 1, 0, 0, 0, 0, time.Local), 2_694_00},
+		{time.Date(2020, 5, 2, 0, 0, 0, 0, time.Local), 19_294_00},
+		{time.Date(2020, 7, 1, 0, 0, 0, 0, time.Local), 395_294_00},
+		{time.Date(2020, 9, 1, 0, 0, 0, 0, time.Local), 102_00},
+		{time.Date(2020, 11, 1, 0, 0, 0, 0, time.Local), 295_00},
+		{time.Date(2020, 4, 11, 0, 0, 0, 0, time.Local), 12_496_00},
+		{time.Date(2020, 3, 20, 0, 0, 0, 0, time.Local), 46_094_00},
+		{time.Date(2020, 3, 21, 0, 0, 0, 0, time.Local), 294_00},
+		{time.Date(2020, 1, 1, 0, 0, 0, 0, time.Local), 13_285_00},
+	}
 
+	splitTranz := make(map[string][]transactions.Transaction)
 
-	sort.SliceStable(transactionsOfService, func(i, j int) bool {
-		return transactionsOfService[i].Date < transactionsOfService[j].Date })
-	sum := transactions.SumConcurrently(transactionsOfService,5)
-	fmt.Println(sum)
+	for _, v := range tranz {
+		splitTranz[v.Date.Format("2006-01")] = append(splitTranz[v.Date.Format("2006-01")], v)
+	}
 
-
+	a := transactions.SumConcurrently(splitTranz,12)
+	fmt.Println("Total:", a)
 
 }
 
--- pkg/transactions/transactions.go
+++ pkg/transactions/transactions.go
@@ -2,99 +2,44 @@ package transactions
 
 import (
 	"fmt"
+	"strconv"
 	"sync"
 	"time"
 )
 
-type TransactionInfo struct {
-	Date int64
-	SumOfTransaction int64
+type Transaction struct {
+	Date time.Time
+	Amount int64
 }
 
-func DateAdding(iStartPosition int64, iMaxPosition int64, slice []TransactionInfo, month time.Month)  {
-	for i := iStartPosition; i < iMaxPosition; i++ {
-		slice[i] = TransactionInfo{
-			Date: dateOfTransaction(month),
-			SumOfTransaction: 1_00,
-		}
-	}
-	return
-}
-
-func dateOfTransaction(month time.Month) int64 {
-	t := time.Date(2020, month, 01, 00, 0, 0, 0, time.Local)
-	unix := t.Unix()
-	return unix
-}
-
-func SumConcurrently(transactions []TransactionInfo, goroutines int) []int64 {
+func SumConcurrently(transactions map[string][]Transaction, goroutines int) int64 { //12 goroutines
 	wg := sync.WaitGroup{}
 	wg.Add(goroutines)
 
-	total := []int64{}
-	index := 0
-	point := 0
-	k := 0
-	lengthOfFirstMonth := transactions//[]TransactionInfo
-	lengthOfSecondMonth := transactions
-	lengthOfThirdMonth := transactions
-	lengthOfThourthMonth := transactions
-	lengthOfThifthMonth := transactions
-
-
-	for _, m := range transactions {
-		index++
-
-		if k == 0 && transactions[point].Date != m.Date {
-			lengthOfFirstMonth = lengthOfFirstMonth[point : index - 1]
-			point = index
-			k++
-		}
-		if k == 1 && transactions[point].Date != m.Date {
-			lengthOfSecondMonth = lengthOfSecondMonth[point : index]
-			point = index
-			k++
-		}
-		if k == 2 && transactions[point].Date != m.Date {
-			lengthOfThirdMonth = lengthOfThirdMonth[point : index]
-			point = index
-			k++
-		}
-		if k == 3 && transactions[point].Date != m.Date {
-			lengthOfThourthMonth = lengthOfThourthMonth[point : index]
-			point = index
-			k++
-		}
-		if k == 4 && transactions[point].Date != m.Date {
-			lengthOfThifthMonth = lengthOfThifthMonth[point : index]
-			point = index
-			k++
-		}
-	}
+	total := int64(0)
 	for i := 0; i < goroutines; i++ {
 		// ВАЖНО: просто с partSize не прокатит, вам нужно как-то заранее разделить слайс по месяцам
 		// ВАЖНО: этот код - лишь шаблон, который показывает вам как запустить горутину
-		var part []TransactionInfo
-		switch i {
-		case 0:
-			part = lengthOfFirstMonth
-		case 1:
-			part = lengthOfSecondMonth
-		case 2:
-			part = lengthOfThirdMonth
-		case 3:
-			part = lengthOfThourthMonth
-		case 4:
-			part = lengthOfThifthMonth
-		default:
-			part = transactions[: len(transactions) / goroutines]
-		}
+		var part []Transaction
+		var pointerOnMonth string
+
+			if i < 9 {
+				a := strconv.Itoa(i + 1)
+				part = transactions["2020-0" + a]
+				pointerOnMonth = "2020-0" + a + ":"
+			}
+
+			if i >= 9 {
+				a := strconv.Itoa(i + 1)
+				part = transactions["2020-" + a]
+				pointerOnMonth = "2020-" + a + ":"
+			}
 
 		go func() {
 			sum := sum(part)
-			total = append(total, sum)
-			fmt.Println(sum)
+			fmt.Println(pointerOnMonth, sum)
 			wg.Done()
+			total += sum
 		}()
 	}
 
@@ -102,11 +47,11 @@ func SumConcurrently(transactions []TransactionInfo, goroutines int) []int64 {
 	return total
 }
 
-func sum(transactions []TransactionInfo) int64  {
+
+func sum(transactions []Transaction) int64  {
 	result := int64(0)
 	for _, transaction := range transactions {
-		result +=transaction.SumOfTransaction
+		result += transaction.Amount
 	}
 	return result
 }
-
