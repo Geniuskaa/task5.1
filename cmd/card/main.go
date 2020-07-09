021368dbe06c317ae1cb11358f1516894a864eed
Geniuska Geniusovna <eminizinar@gmail.com>
2020-07-09 13:43:51 +0300

    New func added and some old funcs changed

--- cmd/card/main.go
+++ cmd/card/main.go
@@ -3,36 +3,33 @@ package main
 import (
 	"fmt"
 	"github.com/Geniuskaa/task5.1/pkg/transactions"
-	"time"
 )
 
 func main() {
 
-
-
 	tranz := []transactions.Transaction{
-		{time.Date(2020, 3, 1, 0, 0, 0, 0, time.Local), 20_396_00},
-		{time.Date(2020, 4, 1, 0, 0, 0, 0, time.Local), 245_00},
-		{time.Date(2020, 5, 1, 0, 0, 0, 0, time.Local), 2_694_00},
-		{time.Date(2020, 5, 2, 0, 0, 0, 0, time.Local), 19_294_00},
-		{time.Date(2020, 7, 1, 0, 0, 0, 0, time.Local), 395_294_00},
-		{time.Date(2020, 9, 1, 0, 0, 0, 0, time.Local), 102_00},
-		{time.Date(2020, 11, 1, 0, 0, 0, 0, time.Local), 295_00},
-		{time.Date(2020, 4, 11, 0, 0, 0, 0, time.Local), 12_496_00},
-		{time.Date(2020, 3, 20, 0, 0, 0, 0, time.Local), 46_094_00},
-		{time.Date(2020, 3, 21, 0, 0, 0, 0, time.Local), 294_00},
-		{time.Date(2020, 1, 1, 0, 0, 0, 0, time.Local), 13_285_00},
-	}
-
-	splitTranz := make(map[string][]transactions.Transaction)
-
-	for _, v := range tranz {
-		splitTranz[v.Date.Format("2006-01")] = append(splitTranz[v.Date.Format("2006-01")], v)
-	}
-
-	a := transactions.SumConcurrently(splitTranz,12)
-	fmt.Println("Total:", a)
-
+		{1583020800, 20_396_00},
+		{1585699200, 245_00},
+		{1588291200, 2_694_00},
+		{1588377600, 19_294_00},
+		{1593561600, 395_294_00},
+		{1598918400, 102_00},
+		{1604188800, 295_00},
+		{1586563200, 12_496_00},
+		{1584662400, 46_094_00},
+		{1584748800, 294_00},
+		{1577836800, 13_285_00},
 }
 
 
+	mapOfTranz, sliceOfSums := transactions.ConvertFromTransactionsSliceToMap(tranz)
+
+
+
+	fmt.Println(sliceOfSums)
+	fmt.Println(mapOfTranz)
+	fmt.Println(len(mapOfTranz))
+
+	fmt.Println("Total:",transactions.SumConcurrently(sliceOfSums,6))
+
+}
--- pkg/transactions/transactions.go
+++ pkg/transactions/transactions.go
@@ -8,38 +8,48 @@ import (
 )
 
 type Transaction struct {
-	Date time.Time
+	Date int64
 	Amount int64
 }
 
-func SumConcurrently(transactions map[string][]Transaction, goroutines int) int64 { //12 goroutines
+
+func ConvertFromTransactionsSliceToMap(slice []Transaction) (map[string][]int64, [][]int64) {
+	mapOfTransactions := make(map[string][]int64)
+	//sliceOfSumTransaction := []int64{}
+
+	for _, v := range slice {
+		t := time.Unix(v.Date, 0)
+		year := strconv.Itoa(t.Year())
+		month := t.Month().String()
+		mapOfTransactions[year + "," + month] = append(mapOfTransactions[year + "," + month], v.Amount)
+	}
+
+	a := [][]int64{}
+	for _,l := range mapOfTransactions {
+		a = append(a, l)
+	}
+
+	return mapOfTransactions, a
+}
+
+func SumConcurrently(transactions [][]int64, goroutines int) int64 {
+	if goroutines > len(transactions) {
+		goroutines = len(transactions)
+	}
 	wg := sync.WaitGroup{}
 	wg.Add(goroutines)
 
 	total := int64(0)
+	//partSize := len(transactions)
 	for i := 0; i < goroutines; i++ {
 		// ВАЖНО: просто с partSize не прокатит, вам нужно как-то заранее разделить слайс по месяцам
 		// ВАЖНО: этот код - лишь шаблон, который показывает вам как запустить горутину
-		var part []Transaction
-		var pointerOnMonth string
-
-			if i < 9 {
-				a := strconv.Itoa(i + 1)
-				part = transactions["2020-0" + a]
-				pointerOnMonth = "2020-0" + a + ":"
-			}
-
-			if i >= 9 {
-				a := strconv.Itoa(i + 1)
-				part = transactions["2020-" + a]
-				pointerOnMonth = "2020-" + a + ":"
-			}
-
+		part := transactions[i]
 		go func() {
 			sum := sum(part)
-			fmt.Println(pointerOnMonth, sum)
-			wg.Done()
 			total += sum
+			fmt.Println(sum)
+			wg.Done()
 		}()
 	}
 
@@ -48,10 +58,11 @@ func SumConcurrently(transactions map[string][]Transaction, goroutines int) int6
 }
 
 
-func sum(transactions []Transaction) int64  {
+func sum(transactions []int64 ) int64  {
 	result := int64(0)
 	for _, transaction := range transactions {
-		result += transaction.Amount
+		result += transaction
 	}
 	return result
 }
+
